2c6027bb5dfac48417952a897946ffd90d027e12
abdulawularman <abdulawularman5114@gmail.com>
2024-12-04 09:29:08 +0600

    separate branch of js

--- JavaScript/for_of_loop.js
+++ JavaScript/for_of_loop.js
@@ -5,7 +5,11 @@
 const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
 
 // your code goes here
-for (let day of days) {
+// for (let day of days) {
 
-    console.log(`${day.charAt(0).toUpperCase()}${day.slice(1)}`);
+//     console.log(`${day.charAt(0).toUpperCase()}${day.slice(1)}`);
+// }
+
+for (let day of days) {
+    console.log(day);
 }
